ed5b2fc09c5441f6823b0e8e4e38d5a83de42adb
EverNorif <1320346985@qq.com>
2025-03-29 00:13:39 +0800

    running entry and readme.

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # gs_toolkit
-toolkit for gaussian-splatting.
+
+This repository provides some useful tools for [gaussian-splatting](https://github.com/graphdeco-inria/gaussian-splatting).
+
+## Installation
+
+```bash
+# Clone the repository with submodules
+git clone --recursive https://github.com/EverNorif/gs_toolkit.git
+cd gs_toolkit
+
+# Create and activate conda environment
+bash setup.sh
+```
+
+## Data Preparation
+
+We use the output results of [vanilla 3DGS](https://github.com/graphdeco-inria/gaussian-splatting) as our input. You can run the official repo to get the reconstruction data. You can also use the reconstruction result of the bicycle scene from [MipNeRF-360](https://jonbarron.info/mipnerf360/) as an example. Download it [here](https://drive.google.com/drive/folders/1mxp4tr8YtZL4tVK8k0MC3CEmgvmbezat?usp=sharing).
+
+```shell
+data/
+├── bicycle/
+│ ├── cameras.json
+│ ├── point_cloud/
+│ │ ├── iteration_xxx/
+│ │ │ └── point_cloud.ply
+│ │ └── .../
+│ └── ...
+└── ...
+```
+
+## Usage
+
+We provide both Python and shell interfaces for running the toolkit.
+
+### Camera Rendering
+
+Camera Rendering will render images at each camera position defined in `cameras.json`.
+
+- python
+
+```python
+from gs_toolkit.entry import Entry
+
+entry = Entry(scene_path='./data/bicycle', output_path='./output')
+entry.render_cameras()
+```
+
+- bash
+
+```shell
+
+
+```
+
+### Video Rendering
+
+Video Rendering will render a surrounding video of the scene like the following result.
+
+(Note: Demo video is compressed. The actual rendered videos will be at full resolution)
+
+- python
+
+```python
+from gs_toolkit.entry import Entry
+
+entry = Entry(scene_path='./data/bicycle', output_path='./output')
+entry.render_video()
+```
+
+- shell
+
+```shell
+
+
+```
+
+## Additional Information
+
+### Camera Conventions
+
+There are some differences between OpenCV camera and the [Camera](https://github.com/graphdeco-inria/gaussian-splatting/blob/main/scene/cameras.py) used in the official 3DGS implementation. Here are some notes:
+
+- The rotation and translation in `cameras.json` are in camera-to-world (c2w) format.
+
+- `getWorld2View2` can get the world-to-camera (w2c) transformation for OpenCV cameras, which can be inverted to get camera-to-world (c2w).
+
+- `gs_toolkit.utils.general_utils.convert_gs_camera_to_cv2_camera` and `gs_toolkit.utils.general_utils.convert_to_camera_model`
+provide conversion methods between OpenCV cameras and 3DGS cameras.
+
+## Acknowledge
+
+Some code in this repo is borrowed from [2DGS](https://github.com/hbb1/2d-gaussian-splatting), [nerfstudio](https://github.com/nerfstudio-project/nerfstudio), thanks their great work.
---
+++ gs_toolkit/entry.py
@@ -0,0 +1,70 @@
+import os
+import fire
+
+import torch
+import torchvision
+from typing import List
+
+from .model.camera_model import Camera
+from .model.gaussian_model import GaussianModel
+from .render.gaussian_render import render
+from .utils.general_utils import searchForMaxIteration, parse_cameras_json
+from .utils.render_utils import generate_bounding_trajectory, save_images_to_video
+from .utils.rich_utils import get_progress, get_status, CONSOLE
+
+class Entry:
+    def __init__(self, scene_path: str, output_path: str, bg_color: torch.Tensor=[1.0, 1.0, 1.0]):
+        self.scene_path = scene_path
+        self.camera_path = os.path.join(scene_path, "cameras.json")
+
+        ply_dir = os.path.join(scene_path, "point_cloud")
+        max_iteration = searchForMaxIteration(ply_dir)
+        self.ply_path = os.path.join(ply_dir, f"iteration_{max_iteration}", "point_cloud.ply")
+
+        scene_name = os.path.basename(scene_path)
+        self.output_path = os.path.join(output_path, scene_name)
+        os.makedirs(self.output_path, exist_ok=True)
+
+        if isinstance(bg_color, list):
+            self.bg_color = torch.tensor(bg_color).cuda()
+        else:
+            self.bg_color = bg_color.cuda()
+        self.gs_model = self._load_gs_model()
+        self.cameras = parse_cameras_json(self.camera_path)
+
+    def _load_gs_model(self) -> GaussianModel:
+        CONSOLE.print(f"[bright_green]:cloud_with_tornado: Load gs model from {self.ply_path}")
+        with get_status(f"[bright_green] Loading model...") as status:
+            gs_model = GaussianModel()
+            gs_model.load_ply(self.ply_path)
+        return gs_model
+
+    def render_cameras(self):
+        save_dir = os.path.join(self.output_path, "renders")
+        os.makedirs(save_dir, exist_ok=True)
+        self.__save_render_results(cameras=self.cameras, save_dir=save_dir, msg=":camera_with_flash: Rendering cameras")
+        CONSOLE.print("[bright_green]:party_popper: Rendering cameras has been saved to: ", save_dir)
+
+    def render_video(self, frame_nums: int=480, fps: int=30):
+        save_dir = os.path.join(self.output_path, "trajectory")
+        os.makedirs(save_dir, exist_ok=True)
+
+        cameras_trajectory = generate_bounding_trajectory(self.cameras, frame_nums)
+        self.__save_render_results(cameras=cameras_trajectory, save_dir=save_dir, msg=":ringed_planet: Rendering trajectory")
+        save_images_to_video(image_folder=save_dir, output_path=os.path.join(self.output_path, "trajectory.mp4"))
+    
+    def render_ellipsoid(self):
+        save_dir = os.path.join(self.output_path, "ellipsoid")
+        os.makedirs(save_dir, exist_ok=True)
+    
+    def __save_render_results(self, cameras: List[Camera], save_dir: str, msg: str):
+        with get_progress(description=f"[bright_green]{msg}...", suffix="frame/s") as progress:
+            for i, camera in enumerate(progress.track(cameras, total=len(cameras))):
+                render_res = render(viewpoint_camera=camera, pc=self.gs_model, bg_color=self.bg_color)
+                save_path = os.path.join(save_dir, f"{i:05d}.png")
+                torchvision.utils.save_image(render_res['render'], save_path)
+                torch.cuda.empty_cache()
+
+
+if __name__ == "__main__":
+    fire.Fire(Entry)
